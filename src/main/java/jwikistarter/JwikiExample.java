3d89f10248f533f77eaf799c95ba92faf9705bba
fastily <fastily@users.noreply.github.com>
2018-03-20 16:40:17 -0700

    initial commit

---
+++ src/main/java/jwikistarter/JwikiExample.java
@@ -0,0 +1,14 @@
+package jwikistarter;
+
+import fastily.jwiki.core.*;
+import fastily.jwiki.dwrap.*;
+
+public class JwikiExample
+{
+	public static void main(String[] args)
+	{
+		Wiki wiki = new Wiki("en.wikipedia.org");
+		
+		// EXAMPLE CODE GOES HERE
+	}
+}
